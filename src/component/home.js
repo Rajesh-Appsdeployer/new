43d20635ad9fd0af070516d4310986261dc1325e
Rajesh <rajesh11022002@gmail.com>
2023-07-25 17:42:03 +0530

    ssr done

---
+++ src/component/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.css';
+const home = () => {
+    return (
+        <div className="home">
+            <p>ssr css checking</p>
+        </div>
+    );
+};
+
+export default home;
